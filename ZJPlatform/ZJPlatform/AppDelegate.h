8f5f744f5fd9e63a3fa78fac808173b9b1ed28e0
fengke <17067636@qq.com>
2018-03-21 14:46:42 +0800

    项目创建

---
+++ ZJPlatform/ZJPlatform/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJPlatform
+//
+//  Created by sostag on 2018/3/21.
+//  Copyright © 2018年 wzsd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZJPlatform/ZJPlatform/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJPlatform
+//
+//  Created by sostag on 2018/3/21.
+//  Copyright © 2018年 wzsd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
