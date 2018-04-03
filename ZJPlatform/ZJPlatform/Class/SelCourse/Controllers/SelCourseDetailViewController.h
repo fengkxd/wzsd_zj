029e623f1ba531bc509716d5cb99d296f631382c
fengkxd <17067636@qq.com>
2018-04-03 17:40:26 +0800

    no message

---
+++ ZJPlatform/ZJPlatform/Class/SelCourse/Controllers/SelCourseDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelCourseDetailViewController.h
+//  ZJPlatform
+//
+//  Created by sostag on 2018/4/3.
+//  Copyright © 2018年 wzsd. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface SelCourseDetailViewController : BaseViewController
+
+@end
